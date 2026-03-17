c55a0796127818a02f9afbb395404135d0c8247a
Helmer <helmer.dendekker@beterbed.net>
2026-03-17 09:11:05 +0100

    Update to ten, code fixes and nuget package update

--- src/Helmer.Helper.ClientGenerator/Program.cs
+++ src/Helmer.Helper.ClientGenerator/Program.cs
@@ -1,65 +1,56 @@
 ﻿using NSwag;
 using NSwag.CodeGeneration.CSharp;
 
-//ToDo: These values from appsettings
-
 // The project where the client needs to be generated in
-string integrationDirectory = "Helmer.Demo.PetStore.Integration";
+const string integrationDirectory = "Helmer.Demo.PetStore.Integration";
 
 // The folder where the client needs to be generated in
-string clientDirectory = "PetStore";
+const string clientDirectory = "PetStore";
 
 // Class Name
-string clientName = "PetStoreApiClient";
+const string clientName = "PetStoreApiClient";
 
 // Root documentation directory
-string documentationDirectory = ".documentation";
-
-string yamlRootDirectory = "petstore";
-
-string openapiFileName = "openapi.yaml";
-
+const string documentationDirectory = ".documentation";
+const string yamlRootDirectory = "petstore";
+const string openapiFileName = "openapi.yaml";
 
 var rootDirectory = Path.GetFullPath(".");
 
-while (!Directory.Exists(Path.Combine(rootDirectory, documentationDirectory)))
-{
-	rootDirectory = Path.GetDirectoryName(rootDirectory);
-	if (rootDirectory == null)
-	{
-		throw new FileNotFoundException("Could not find the root directory.");
-	}
-}
+while (rootDirectory != null && !Directory.Exists(Path.Combine(rootDirectory, documentationDirectory)))
+    rootDirectory = Path.GetDirectoryName(rootDirectory);
 
-string path = Path.Combine(rootDirectory, documentationDirectory, yamlRootDirectory, openapiFileName);
+if (rootDirectory == null)
+    throw new FileNotFoundException("Could not find the root directory.");
+
+var path = Path.Combine(rootDirectory, documentationDirectory, yamlRootDirectory, openapiFileName);
 
 var document = await OpenApiYamlDocument.FromFileAsync(path);
 
-
 var settings = new CSharpClientGeneratorSettings
 {
-	GenerateClientInterfaces = true,
-	ClassName = clientName,
-	InjectHttpClient = true,
-	UseBaseUrl = false,
-	CSharpGeneratorSettings =
-	{
-		Namespace = $"{integrationDirectory}.{clientDirectory}"
-	}
+    GenerateClientInterfaces = true,
+    ClassName = clientName,
+    InjectHttpClient = true,
+    UseBaseUrl = false,
+    CSharpGeneratorSettings =
+    {
+        Namespace = $"{integrationDirectory}.{clientDirectory}"
+    }
 };
 
 var generator = new CSharpClientGenerator(document, settings);
 var code = generator.GenerateFile();
 
-
-//ToDo see if you can split the generated files
-
 var outputDirectory = Path.Combine(rootDirectory, "src", integrationDirectory, clientDirectory);
+
+if (!Directory.Exists(outputDirectory))
+    Directory.CreateDirectory(outputDirectory);
+
 var allOneFile = Path.Combine(outputDirectory, $"{clientName}.cs");
 
-if (File.Exists(allOneFile)) File.Delete(allOneFile);
+if (File.Exists(allOneFile))
+    File.Delete(allOneFile);
 
-using (StreamWriter streamwriter = File.AppendText(allOneFile))
-{
-	streamwriter.Write(code);
-}
+using var streamwriter = File.AppendText(allOneFile);
+streamwriter.Write(code);
