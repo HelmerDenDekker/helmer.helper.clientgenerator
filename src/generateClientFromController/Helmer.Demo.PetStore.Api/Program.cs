545264ae7ff6ccf409464a88b4439a0731cd1786
Helmer <helmer.dendekker@beterbed.net>
2026-03-17 10:18:56 +0100

    something goes wrong, creates extra fields

--- src/generateClientFromController/Helmer.Demo.PetStore.Api/Program.cs
+++ src/generateClientFromController/Helmer.Demo.PetStore.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
@@ -12,6 +13,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+    
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
