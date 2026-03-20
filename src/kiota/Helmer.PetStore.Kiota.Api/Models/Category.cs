394982332e8200795752a3bfbfcd4e4afeb768cb
Helmer <helmer.dendekker@beterbed.net>
2026-03-20 15:55:38 +0100

    kiota start

---
+++ src/kiota/Helmer.PetStore.Kiota.Api/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace Helmer.PetStore.Kiota.Api.Models;
+
+public class Category
+{
+    public long? Id { get; set; }
+    public string? Name { get; set; }
+}
+
---
+++ src/kiota/Helmer.PetStore.Kiota.Api/Models/Tag.cs
@@ -0,0 +1,8 @@
+namespace Helmer.PetStore.Kiota.Api.Models;
+
+public class Tag
+{
+    public long? Id { get; set; }
+    public string? Name { get; set; }
+}
+
